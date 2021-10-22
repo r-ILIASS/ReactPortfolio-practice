55fabcc118620f9d6bca0a7ddb2bd3c152ecbb58
r-ILIASS <iliass.root@gmail.com>
2021-10-22 18:30:23 +0100

    full base functionality

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
 import "./contact.scss";
 
 export default function Contact() {
-  return <div className="contact" id="contact"></div>;
+  const [message, setMessage] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setMessage(true);
+  };
+
+  return (
+    <div className="contact" id="contact">
+      <div className="left">
+        <img src="assets/shake.svg" alt="" />
+      </div>
+      <div className="right">
+        <h2>Contact.</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="email" />
+          <textarea placeholder="Message"></textarea>
+          <button type="submit">Send</button>
+          {message && (
+            <span>Thank you for submitting a message, we'll reply soon</span>
+          )}
+        </form>
+      </div>
+    </div>
+  );
 }
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,41 +1,81 @@
+import { useState, useEffect } from "react";
+import PortfolioList from "./portfolioList/PortfolioList";
+import {
+  featuredPortfolio,
+  webPortfolio,
+  mobilePortfolio,
+  contentPortfolio,
+} from "../../data.js";
+
 import "./portfolio.scss";
 
 export default function Portfolio() {
+  const [selected, setSelected] = useState("featured");
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    switch (selected) {
+      case "featured":
+        setData(featuredPortfolio);
+        break;
+      case "web":
+        setData(webPortfolio);
+        break;
+      case "mobile":
+        setData(mobilePortfolio);
+        break;
+      case "content":
+        setData(contentPortfolio);
+        break;
+      default:
+        setData(featuredPortfolio);
+        break;
+    }
+  }, [selected]);
+
+  const list = [
+    {
+      id: "featured",
+      title: "Featured",
+    },
+    {
+      id: "web",
+      title: "Web App",
+    },
+    {
+      id: "mobile",
+      title: "Mobile App",
+    },
+    {
+      id: "design",
+      title: "Design",
+    },
+    {
+      id: "content",
+      title: "Content",
+    },
+  ];
+
   return (
     <div className="portfolio" id="portfolio">
       <h1>Portfolio</h1>
       <ul>
-        <li className="active">Featured</li>
-        <li>Web App</li>
-        <li>Mobile App</li>
-        <li>Design</li>
-        <li>Branding</li>
+        {list.map((item) => (
+          <PortfolioList
+            title={item.title}
+            id={item.id}
+            active={selected === item.id}
+            setSelected={setSelected}
+          />
+        ))}
       </ul>
       <div className="container">
-        <div className="item">
-          <img src="assets/img/placeholder.png" alt="" />
-          <h3>Banking</h3>
-        </div>
-        <div className="item">
-          <img src="assets/img/placeholder.png" alt="" />
-          <h3>Banking</h3>
-        </div>
-        <div className="item">
-          <img src="assets/img/placeholder.png" alt="" />
-          <h3>Banking</h3>
-        </div>
-        <div className="item">
-          <img src="assets/img/placeholder.png" alt="" />
-          <h3>Banking</h3>
-        </div>
-        <div className="item">
-          <img src="assets/img/placeholder.png" alt="" />
-          <h3>Banking</h3>
-        </div>
-        <div className="item">
-          <img src="assets/img/placeholder.png" alt="" />
-          <h3>Banking</h3>
-        </div>
+        {data.map((dataitem) => (
+          <div className="item">
+            <img src={dataitem.img} alt="" />
+            <h3>{dataitem.title}</h3>
+          </div>
+        ))}
       </div>
     </div>
   );
